346b0b71ccdb0b44901ae4b2303dbc7633556e8d
wwgswqg <2710651793@qq.com>
2022-08-09 12:08:55 +0800

    add muduo eventnet

--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "google/protobuf/service.h"
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+#include <muduo/net/TcpConnection.h>
+#include <string>
+#include <functional>
+#include <google/protobuf/descriptor.h>
+#include <unordered_map>
 
 // 框架提供的专门发布rpc服务的网络对象类
 class RpcProvider
@@ -10,4 +18,24 @@ public:
 
     // 启动rpc服务节点，开始提供rpc远程网络调用服务
     void Run();
+
+private:
+    // 组合EventLoop
+    muduo::net::EventLoop m_eventLoop;
+
+    // service服务类型信息
+    struct ServiceInfo
+    {
+        google::protobuf::Service *m_service; // 保存服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> m_methodMap; // 保存服务方法
+    };
+    // 存储注册成功的服务对象和其服务方法的所有信息
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
+
+    // 新的socket连接回调
+    void OnConnection(const muduo::net::TcpConnectionPtr&);
+    // 已建立连接用户的读写事件回调
+    void OnMessage(const muduo::net::TcpConnectionPtr&, muduo::net::Buffer*, muduo::Timestamp);
+    // Closure的回调操作，用于序列化rpc的响应和网络发送
+    void SendRpcResponse(const muduo::net::TcpConnectionPtr&, google::protobuf::Message*);
 };
--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -16,5 +16,44 @@ void RpcProvider::NotifyService(google::protobuf::Service *service)
 // 启动rpc服务节点，开始提供rpc远程网络调用服务
 void RpcProvider::Run()
 {
-    
+    // 读取配置文件rpcserver的信息
+    std::string ip = MprpcApplication::GetInstance().GetConfig().Load("rpcserverip");
+    uint16_t port = atoi(MprpcApplication::GetInstance().GetConfig().Load("rpcserverport").c_str());
+    muduo::net::InetAddress address(ip, port);
+
+    // 创建TcpServer对象
+    muduo::net::TcpServer server(&m_eventLoop, address, "RpcProvider");
+
+    // 绑定连接回调和消息读写回调方法  分离了网络代码和业务代码
+    server.setConnectionCallback(std::bind(&RpcProvider::OnConnection, this, std::placeholders::_1));
+    server.setMessageCallback(std::bind(&RpcProvider::OnMessage, this, std::placeholders::_1, 
+            std::placeholders::_2, std::placeholders::_3));
+
+    // 设置muduo库的线程数量
+    server.setThreadNum(4);
+
+    // rpc服务端准备启动，打印信息
+    std::cout << "RpcProvider start service at ip:" << ip << " port:" << port << std::endl;
+ 
+    // 启动网络服务
+    server.start();
+    m_eventLoop.loop(); 
+}
+
+// 新的socket连接回调
+void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr &conn)
+{
+    if (!conn->connected())
+    {
+        // 和rpc client的连接断开了
+        conn->shutdown();
+    }
+}
+
+// 已建立连接用户的读写事件回调 如果远程有一个rpc服务的调用请求，那么OnMessage方法就会响应
+void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr &conn, 
+                            muduo::net::Buffer *buffer, 
+                            muduo::Timestamp)
+{
+
 }
