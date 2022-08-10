e6f3f4d09a229e2f0255c9c545dab78d9a4761a8
wwgswqg <2710651793@qq.com>
2022-08-10 16:01:00 +0800

    add mprpccontroller to show status

--- example/caller/callfriendservice.cc
+++ example/caller/callfriendservice.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "mprpcapplication.h"
 #include "friend.pb.h"
-#include "mprpcchannel.h"
 
 int main(int argc, char **argv)
 {
@@ -16,24 +15,30 @@ int main(int argc, char **argv)
     // rpc方法的响应
     fixbug::GetFriendsListResponse response;
     // 发起rpc方法的调用  同步的rpc调用过程  MprpcChannel::callmethod
-  
-    stub.GetFriendsList(nullptr,&request, &response, nullptr); // RpcChannel->RpcChannel::callMethod 集中来做所有rpc方法调用的参数序列化和网络发送
+    MprpcController controller;
+    stub.GetFriendsList(&controller,&request, &response, nullptr); // RpcChannel->RpcChannel::callMethod 集中来做所有rpc方法调用的参数序列化和网络发送
 
-
-    if (0 == response.result().errcode())
+    // 一次rpc调用完成，读调用的结果
+    if (controller.Failed())
     {
-        std::cout << "rpc GetFriendsList response success!" << std::endl;
-        int size = response.friends_size();
-        for (int i=0; i < size; ++i)
-        {
-            std::cout << "index:" << (i+1) << " name:" << response.friends(i) << std::endl;
-        }
+        std::cout << controller.ErrorText() << std::endl;
     }
     else
     {
-        std::cout << "rpc GetFriendsList response error : " << response.result().errmsg() << std::endl;
+        if (0 == response.result().errcode())
+        {
+            std::cout << "rpc GetFriendsList response success!" << std::endl;
+            int size = response.friends_size();
+            for (int i=0; i < size; ++i)
+            {
+                std::cout << "index:" << (i+1) << " name:" << response.friends(i) << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "rpc GetFriendsList response error : " << response.result().errmsg() << std::endl;
+        }
     }
     
-
     return 0;
 }
--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "mprpcconfig.h"
+#include "mprpcchannel.h"
+#include "mprpccontroller.h"
 // mprpc框架的基础类，负责框架的一些初始化操作
 class MprpcApplication
 {
